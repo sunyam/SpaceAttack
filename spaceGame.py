ad41695d235706765d1b625dc519b25c82d1e0b6
Sunyam Bagga <sunyambagga@gmail.com>
2016-02-06 21:36:31 +0530

    Adding Sprite class

--- spaceGame.py
+++ spaceGame.py
@@ -155,5 +155,55 @@ class Ship:
     def get_radius(self):
         return self.radius
     
+# Sprite class
+class Sprite:
+    def __init__(self, pos, vel, ang, ang_vel, image, info, sound = None):
+        self.pos = [pos[0],pos[1]]
+        self.vel = [vel[0],vel[1]]
+        self.angle = ang
+        self.angle_vel = ang_vel
+        self.image = image
+        self.image_center = info.get_center()
+        self.image_size = info.get_size()
+        self.radius = info.get_radius()
+        self.lifespan = info.get_lifespan()
+        self.animated = info.get_animated()
+        self.age = 0
+        if sound:
+            sound.rewind()
+            sound.play()
+   
+    def draw(self, canvas):
+        canvas.draw_image(self.image, self.image_center, self.image_size,
+                          self.pos, self.image_size, self.angle)
+
+    def update(self):
+        self.age += 1
+        # update angle
+        self.angle += self.angle_vel
+        
+        # update position
+        self.pos[0] = (self.pos[0] + self.vel[0]) % WIDTH
+        self.pos[1] = (self.pos[1] + self.vel[1]) % HEIGHT
+        
+        # life of a missile
+        if self.age >= self.lifespan:
+            return True
+        else:
+            return False
+        
+    def get_position(self):
+        return self.pos
+    
+    def get_radius(self):
+        return self.radius
+
+    def collide(self, other_object):
+        if dist(self.get_position(), other_object.get_position()) > self.get_radius() + other_object.get_radius():
+            return False
+        else:
+            return True
+    
+
 
 
